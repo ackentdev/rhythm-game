f44fd544b0a48c03d8bc14e73880ec8b2dcab476
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-07-02 16:14:16 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './Reset.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="mainTitle">
+        <h1>Avengers:</h1>
+        <p>Loki Can't Stop the Beat</p>
+      </div>
+      <p>Link goes here that takes student to instructions page</p>
     </div>
   );
 }
