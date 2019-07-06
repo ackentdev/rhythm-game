c75fcecef3ced86f499ac2f2980b1336de3e8a92
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-07-06 10:44:34 -0700

    moving files to server

--- src/components/EnemyWindow.js
+++ src/components/EnemyWindow.js
@@ -2,6 +2,7 @@ import React from "react";
 import quarter4 from "../media/enemyLibrary/quarter4.png";
 
 export default function EnemyWindow() {
+    const mappedEnemies = 
         return(
             <img src={quarter4}/>
         );
