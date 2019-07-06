c8517eaf3e5f1a03dbeadedfad22384e741cc8e6
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-07-06 11:38:05 -0700

    tried strict api request

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const { getProblems } = require("./controller/inputController");
 
 app.use(express.json());
 
-app.get("/api/problems", getProblems)
+app.get("http://localhost:3333/api/problems", getProblems)
 
 app.listen(port, () => 
 console.log(`Server listening on ${port}`));
